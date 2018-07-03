5341a7f6479c71b1e80935ba638ec3068cc20ba0
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-03 11:01:34 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,36 @@
+class Author
+  attr_accessor :name
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @title = []
+  end
+
+  def add_title(title)
+    @title << title
+    title.author = self
+    @@post_count +=1
+  end
+
+  def title
+    @title
+  end
+
+  def add_post_by_title(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    song_count +=1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+  def author_name
+    self.author.name
+  end
+
+end
