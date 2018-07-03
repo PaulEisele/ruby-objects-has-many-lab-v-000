e11d35fda4b5a51628ea8b6bfabade911355e565
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-03 10:41:34 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  
+  attr_accessor :artist, :name, :genre
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
+
+  def artist_name
+    self.artist.name
+  end
+  
+end
